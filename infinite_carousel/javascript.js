cff7c7471c0416172c76c18a0b51241acd716076
NhatLeNguyen <nhatlenguyen843@gmail.com>
2024-07-22 11:01:37 +0700

    update dot n responsive

--- infinite_carousel/javascript.js
+++ infinite_carousel/javascript.js
@@ -1,13 +1,15 @@
 const carousel = document.querySelector(".carousel-slides");
 const slides = Array.from(document.querySelectorAll(".carousel-item"));
+const dots = Array.from(document.querySelectorAll(".dots"));
 
 const left = document.querySelector(".fa-arrow-left");
 const right = document.querySelector(".fa-arrow-right");
 let currentIndex = 0;
+let slidesToShow = 4;
 
 function updateCarousel() {
   carousel.innerHTML = "";
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < slidesToShow; i++) {
     let slideIndex = (currentIndex + i) % slides.length;
     let slideClone = slides[slideIndex].cloneNode(true);
     carousel.appendChild(slideClone);
@@ -17,23 +19,39 @@ function updateCarousel() {
 left.addEventListener("click", () => {
   currentIndex = (currentIndex - 1 + slides.length) % slides.length;
   carousel.style.transition = "transform 0.5s ease";
-  carousel.style.transform = `translateX(300px)`;
+  carousel.style.transform = `translateX(250px)`;
   setTimeout(() => {
     carousel.style.transition = "none";
     carousel.style.transform = `translateX(0)`;
     updateCarousel();
-  }, 200);
+  }, 300);
 });
 
 right.addEventListener("click", () => {
   currentIndex = (currentIndex + 1) % slides.length;
   carousel.style.transition = "transform 0.5s ease";
-  carousel.style.transform = `translateX(-300px)`;
+  carousel.style.transform = `translateX(-250px)`;
   setTimeout(() => {
     carousel.style.transition = "none";
     carousel.style.transform = `translateX(0)`;
     updateCarousel();
-  }, 200);
+  }, 300);
 });
 
-updateCarousel();
+dots.forEach((dot) => {
+  dot.addEventListener("click", (e) => {
+    currentIndex = parseInt(e.target.getAttribute("index")) * 4;
+    updateCarousel();
+  });
+});
+function updateSlidesToShow() {
+  if (window.innerWidth < 620) {
+    slidesToShow = 1;
+  } else {
+    slidesToShow = 4;
+  }
+  updateCarousel();
+}
+window.addEventListener("resize", updateSlidesToShow);
+
+updateSlidesToShow();
