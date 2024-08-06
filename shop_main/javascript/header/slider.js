51597b9e2c8437229c2d3efa2dea691fec03d00b
NhatLeNguyen <nhatlenguyen843@gmail.com>
2024-08-06 15:59:53 +0700

    nthin

--- shop_main/javascript/header/slider.js
+++ shop_main/javascript/header/slider.js
@@ -6,7 +6,6 @@ function plusSlides(n) {
 }
 
 function showSlides(n) {
-  let i;
   let slides = document.getElementsByClassName("slide");
   if (n > slides.length) {
     slideIndex = 1;
@@ -14,7 +13,7 @@ function showSlides(n) {
   if (n < 1) {
     slideIndex = slides.length;
   }
-  for (i = 0; i < slides.length; i++) {
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
 
